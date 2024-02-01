1cc62adf72e0754f45ae5a24f2185666ec2b3f0b
Steinar Logi <steinarlg@gmail.com>
2024-02-01 19:14:04 +0100

    Add momentum to the gradient descent for the regression neural network

--- Labs/Lab 1/Lab 1b/Steinar/3.1.3.py
+++ Labs/Lab 1/Lab 1b/Steinar/3.1.3.py
@@ -22,31 +22,31 @@ plt.show()
 dataset = np.c_[x_mesh.ravel(), y_mesh.ravel(), np.ones(len(x_mesh.ravel()))].T
 targets = np.array([Z])
 
-model = DoubleLayerNN(nr_of_hidden_nodes=10, learning_rate=0.00001)
+model = DoubleLayerNN(nr_of_hidden_nodes=10, learning_rate=0.0001)
 mse = model.fit_data(dataset, targets, iterations=10000)
 
-sklearn_model = MLPRegressor(solver='sgd', hidden_layer_sizes=(10), activation='logistic', momentum=0, batch_size=len(dataset.T))
-sklearn_model.fit(dataset.T, targets.T)
-
-print(sklearn_model.loss_)
-
-plt.plot(mse)
-plt.show()
-
-predictions = model.predict(dataset)
-print(np.all(predictions < targets + 0.1) and np.all(predictions > targets - 0.1))
-print(np.max(predictions))
-print(np.min(predictions))
-print(np.max(targets))
-print(np.max(targets))
-
-print((1/predictions.shape[1]) * np.sum((predictions - targets)**2))
-fig, ax = plt.subplots(subplot_kw={"projection": "3d"})
-ax.plot_surface(x_mesh, y_mesh, predictions[0].reshape(x_mesh.shape), color='blue')
-ax.plot_surface(x_mesh, y_mesh, targets.reshape(x_mesh.shape), color='green')
-ax.plot_surface(x_mesh, y_mesh, Z.reshape(x_mesh.shape), color='red')
-plt.show()
-
-print(x_mesh.ravel().shape)
-print(y_mesh.ravel().shape)
-print(predictions[0].shape)
+#sklearn_model = MLPRegressor(solver='sgd', hidden_layer_sizes=(10), activation='logistic', momentum=0, batch_size=len(dataset.T))
+#sklearn_model.fit(dataset.T, targets.T)
+#
+#print(sklearn_model.loss_)
+#
+#plt.plot(mse)
+#plt.show()
+#
+#predictions = model.predict(dataset)
+#print(np.all(predictions < targets + 0.1) and np.all(predictions > targets - 0.1))
+#print(np.max(predictions))
+#print(np.min(predictions))
+#print(np.max(targets))
+#print(np.max(targets))
+#
+#print((1/predictions.shape[1]) * np.sum((predictions - targets)**2))
+#fig, ax = plt.subplots(subplot_kw={"projection": "3d"})
+#ax.plot_surface(x_mesh, y_mesh, predictions[0].reshape(x_mesh.shape), color='blue')
+#ax.plot_surface(x_mesh, y_mesh, targets.reshape(x_mesh.shape), color='green')
+#ax.plot_surface(x_mesh, y_mesh, Z.reshape(x_mesh.shape), color='red')
+#plt.show()
+#
+#print(x_mesh.ravel().shape)
+#print(y_mesh.ravel().shape)
+#print(predictions[0].shape)
--- Labs/Lab 1/Lab 1b/backprop/double_layer_regression_nn.py
+++ Labs/Lab 1/Lab 1b/backprop/double_layer_regression_nn.py
@@ -10,6 +10,7 @@ class DoubleLayerNN:
         self.W_hidden = np.random.normal(0, 1, (self.hidden_nodes, self.input_nodes))
         self.W_output = np.random.normal(0, 1, (self.output_nodes, self.hidden_nodes))
         self.learning_rate = learning_rate
+        self.alpha = 0.9
 
         
     def predict(self, X):
@@ -32,26 +33,34 @@ class DoubleLayerNN:
         assert Y.shape[1] == X.shape[1], 'dimensions of input and output do not match up'
 
         mse = []
+        # For the momentum
+        prev_dW_hidden = np.zeros((self.hidden_nodes, self.input_nodes))
+        prev_dW_output = np.zeros((self.output_nodes, self.hidden_nodes))
         for i in range(iterations):
             # Forward pass
             H_1, A_1, O = self.forward_pass(X)
             # Backwards pass
-            dW_hidden, dW_output = self.compute_gradients(A_1, O, X, Y)
+            dW_hidden, dW_output = self.compute_gradients(A_1, O, X, Y, prev_dW_hidden, prev_dW_output)
             # Update weights
             self.W_hidden = self.W_hidden - self.learning_rate * dW_hidden
             self.W_output = self.W_output - self.learning_rate * dW_output
             # calculate mse
             current_mse = self.calculate_mse(Y, O)
             mse.append(current_mse)
+            # Set previous gradients as current
+            prev_dW_hidden = dW_hidden
+            prev_dW_output = dW_output
 
         return mse
 
-    def compute_gradients(self, A_1, O, X, T):
+    def compute_gradients(self, A_1, O, X, T, prev_dW_hidden, prev_dW_output):
         dA_output = (O - T)
         dW_output = dA_output @ A_1.T
+        dW_output = (1 - self.alpha) * dW_output + self.alpha * prev_dW_output
 
         dA_hidden = self.W_output.T @ (dA_output)
         dW_hidden = (dA_hidden * self.sigmoid_derivative(A_1)) @ X.T
+        dW_hidden = (1 - self.alpha) * dW_hidden + self.alpha * prev_dW_hidden
 
         return dW_hidden, dW_output
     
